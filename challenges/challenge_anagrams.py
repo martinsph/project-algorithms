e269aff003ce9dcbf80300a47779de9cc6b77e00
martinsph <ph.mmartins@gmail.com>
2022-03-29 17:59:53 -0300

    add req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,6 +2,7 @@ def aux(string):
     letter_dict = {}
     for letter in string.lower():
         if letter in letter_dict:
+            # this increment does not add a key, but updates an existing one 
             letter_dict[letter] += 1
         else:
             letter_dict[letter] = 1
@@ -10,13 +11,17 @@ def aux(string):
 
 
 def is_anagram(first_string, second_string):
+    # check if strings are empty
     if first_string == "" or second_string == "":
         return False
 
+    # to make the code faster it checks is the letter lenght are not equal
     if len(first_string) != len(second_string):
         return False
 
+    # calls aux function to create dicts to comparison
     first_dict = aux(first_string)
     second_dict = aux(second_string)
 
+    # if both are equals then, return true, otherwise, false
     return first_dict == second_dict
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,24 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) <= 1:
+        return False
+
+    to_be_compare = 0
+    nums.sort()
+
+    # for index, number in enumerate(nums):
+    #     if not isinstance(number, int) or number < 1:
+    #         return False
+
+    #     if number == nums[index - 1]:
+    #         return number
+
+    for number in nums:
+        if not isinstance(number, int) or number < 1:
+            return False
+
+        if number != to_be_compare:
+            to_be_compare = number
+        else:
+            return number
+
+    return False
