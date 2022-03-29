1ae8a13f515bd9398485fdab5022917689e9f90e
martinsph <ph.mmartins@gmail.com>
2022-03-29 12:08:46 -0300

    add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # check if word is a empty string
+    if word == "":
+        return False
+
+    if len(word) <= 1:
+        return True
+
+    if high_index < low_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
