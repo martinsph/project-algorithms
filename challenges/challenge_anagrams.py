26fa5233acaf6b2391dd9b1c9ee349139f22315c
martinsph <ph.mmartins@gmail.com>
2022-03-29 14:05:55 -0300

    add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
+def aux(string):
+    letter_dict = {}
+    for letter in string.lower():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
+
+
 def is_anagram(first_string, second_string):
-    
+    if first_string == "" or second_string == "":
+        return False
+
+    if len(first_string) != len(second_string):
+        return False
+
+    first_dict = aux(first_string)
+    second_dict = aux(second_string)
+
+    return first_dict == second_dict
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,12 +3,16 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == "":
         return False
 
+    # One letter words are considered palindromes
     if len(word) <= 1:
         return True
 
+    # when the higher index is less than the lower the word is over.
+    # If it has not crashed yet its because is a palindrome.
     if high_index < low_index:
         return True
 
+    # check if letters are equal and use recursion
     if word[low_index] == word[high_index]:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
