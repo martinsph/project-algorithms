0406c9bde40c7155dfbf5e5987584f4179a62914
martinsph <ph.mmartins@gmail.com>
2022-03-29 18:13:45 -0300

    add req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    # check if word is a empty string
+    if word == "":
+        return False
+
+    # One letter words are considered palindromes
+    if len(word) <= 1:
+        return True
+
+    if word == "".join(reversed(word)):
+        return True
+
+    return False
