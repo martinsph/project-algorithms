7667c673d79746614e19905b71f847adabaa080e
martinsph <ph.mmartins@gmail.com>
2022-03-29 11:24:56 -0300

    add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    # testa se é nulo.
+    if target_time is None:
+        return None
+    
+    online_students = 0
+
+    for login, logout in permanence_period:
+        # testa se as entradas são inválidas.
+        if not (
+            isinstance(login, int) 
+            and isinstance(logout, int)
+            ):
+            return None
+
+        # add plus one every time target_time value is between both permanence_period (tuple) values 
+        if login <= target_time <= logout:
+            online_students += 1
+
+    return online_students
